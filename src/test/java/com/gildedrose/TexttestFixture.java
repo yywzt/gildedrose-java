656fb697c5d712c7ba71754d41fcfbbd1d436c07
chengkunxf <chengkunxf@126.com>
2020-02-20 16:49:40 +0800

    refactor:extact createSulfuras and createBackstage method eliminate magic strings

--- src/test/java/com/gildedrose/TexttestFixture.java
+++ src/test/java/com/gildedrose/TexttestFixture.java
@@ -17,12 +17,12 @@ public class TexttestFixture {
                 new Item("+5 Dexterity Vest", 10, 20), //
                 createAgedBrie(2, 0), //
                 new Item("Elixir of the Mongoose", 5, 7), //
-                new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
-                new Item("Sulfuras, Hand of Ragnaros", -1, 80),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 1, 20),
+                createSulfuras(80, 0), //
+                createSulfuras(80, -1),
+                createBackStage(15, 20),
+                createBackStage(10, 49),
+                createBackStage(5, 49),
+                createBackStage(1, 20),
                 // this conjured item does not work properly yet
                 new Item("Conjured Mana Cake", 3, 6) };
 
@@ -38,6 +38,14 @@ public class TexttestFixture {
             app.updateQuality();
         }
         return outputStream.toString();
+    }
+
+    private static Item createBackStage(int sellIn, int quality) {
+        return new Item("Backstage passes to a TAFKAL80ETC concert", sellIn, quality);
+    }
+
+    private static Item createSulfuras(int quality, int sellIn) {
+        return new Item("Sulfuras, Hand of Ragnaros", sellIn, quality);
     }
 
     private static Item createAgedBrie(int sellIn, int quality) {
