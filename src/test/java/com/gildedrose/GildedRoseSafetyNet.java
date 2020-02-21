93b15c1b358e74616788ba3f69aba95e6cf0f4f7
chengkunxf <chengkunxf@126.com>
2020-02-22 01:01:59 +0800

    refactor:建立安全测试网，覆盖率 95%，可以一键运行，方便查看测试报告

---
+++ src/test/java/com/gildedrose/GildedRoseSafetyNet.java
@@ -0,0 +1,20 @@
+package com.gildedrose;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Files;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class GildedRoseSafetyNet {
+
+    @Test
+    public void should_output_string_equals_baseline() throws IOException {
+        String baseline = Files.toString(new File("src/test/baseline.txt"), Charsets.UTF_8);
+        assertThat(TexttestFixture.getOutputString(),is(baseline));
+    }
+}
--- src/test/java/com/gildedrose/TexttestFixture.java
+++ src/test/java/com/gildedrose/TexttestFixture.java
@@ -1,10 +1,19 @@
 package com.gildedrose;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 public class TexttestFixture {
     public static void main(String[] args) {
-        System.out.println("OMGHAI!");
+        System.out.println(getOutputString());
+    }
 
-        Item[] items = new Item[] {
+    public static String getOutputString() {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+        printStream.println("OMGHAI!");
+
+        Item[] items = new Item[]{
                 new Item("+5 Dexterity Vest", 10, 20), //
                 new Item("Aged Brie", 2, 0), //
                 new Item("Elixir of the Mongoose", 5, 7), //
@@ -15,24 +24,21 @@ public class TexttestFixture {
                 new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 1, 20),
                 // this conjured item does not work properly yet
-                new Item("Conjured Mana Cake", 3, 6) };
+                new Item("Conjured Mana Cake", 3, 6)};
 
         GildedRose app = new GildedRose(items);
 
-        int days = 3;
-        if (args.length > 0) {
-            days = Integer.parseInt(args[0]) + 1;
-        }
-
-        for (int i = 0; i < days; i++) {
-            System.out.println("-------- day " + i + " --------");
-            System.out.println("name, sellIn, quality");
+        for (int i = 0; i < 3; i++) {
+            printStream.println("-------- day " + i + " --------");
+            printStream.println("name, sellIn, quality");
             for (Item item : items) {
-                System.out.println(item);
+                printStream.println(item);
             }
-            System.out.println();
+            printStream.println();
             app.update_quality();
         }
+
+        return outputStream.toString();
     }
 
 }
