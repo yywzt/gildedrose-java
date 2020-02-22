aab1a4627756aa46abd7c2a2f0cf06a6b39713a0
chengkunxf <chengkunxf@126.com>
2020-02-22 17:29:32 +0800

    refactor:extract method to eliminate magic strings

--- src/main/java/com/gildedrose/Item.java
+++ src/main/java/com/gildedrose/Item.java
@@ -14,6 +14,69 @@ public class Item {
         this.quality = quality;
     }
 
+    protected void dealQuality() {
+        if (!isAgedBrie()
+                && !isBackstagePass()) {
+            if (quality > 0) {
+                if (!isSulfuras()) {
+                    quality = quality - 1;
+                }
+            }
+        } else {
+            if (quality < 50) {
+                quality = quality + 1;
+
+                if (isBackstagePass()) {
+                    if (sell_in < 11) {
+                        if (quality < 50) {
+                            quality = quality + 1;
+                        }
+                    }
+
+                    if (sell_in < 6) {
+                        if (quality < 50) {
+                            quality = quality + 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        if (!isSulfuras()) {
+            sell_in = sell_in - 1;
+        }
+
+        if (sell_in < 0) {
+            if (!isAgedBrie()) {
+                if (!isBackstagePass()) {
+                    if (quality > 0) {
+                        if (!isSulfuras()) {
+                            quality = quality - 1;
+                        }
+                    }
+                } else {
+                    quality = quality - quality;
+                }
+            } else {
+                if (quality < 50) {
+                    quality = quality + 1;
+                }
+            }
+        }
+    }
+
+    private boolean isSulfuras() {
+        return name.equals("Sulfuras, Hand of Ragnaros");
+    }
+
+    private boolean isBackstagePass() {
+        return name.equals("Backstage passes to a TAFKAL80ETC concert");
+    }
+
+    private boolean isAgedBrie() {
+        return name.equals("Aged Brie");
+    }
+
     @Override
     public String toString() {
         return this.name + ", " + this.sell_in + ", " + this.quality;
