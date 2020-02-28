3e59af08593690c2efe46ec7ce9f094f436a5828
chengkunxf <chengkunxf@126.com>
2020-02-28 10:28:44 +0800

    refactor:create Sulfuras subclss

---
+++ src/main/java/com/gildedrose/Sulfuras.java
@@ -0,0 +1,8 @@
+package com.gildedrose;
+
+public class Sulfuras extends Item {
+
+    public Sulfuras(int sellIn, int quality) {
+        super(SULFURAS_HAND_OF_RAGNAROS, sellIn, quality);
+    }
+}
--- src/test/java/com/gildedrose/TexttestFixture.java
+++ src/test/java/com/gildedrose/TexttestFixture.java
@@ -19,8 +19,8 @@ public class TexttestFixture {
                 new Item("+5 Dexterity Vest", 10, 20), //
                 new AgedBrie(2, 0), //
                 new Item("Elixir of the Mongoose", 5, 7), //
-                new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
-                new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+                new Sulfuras(0, 80), //
+                new Sulfuras(-1, 80),
                 new BackstagePass(15, 20),
                 new BackstagePass(10, 49),
                 new BackstagePass(5, 49),
