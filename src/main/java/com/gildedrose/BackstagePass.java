82bea8900861df75b0bf463991b3f85fc550a3cd
chengkunxf <chengkunxf@126.com>
2020-04-11 00:26:44 +0800

    refactor:create subclass AgedBrie BackstagePass Sulfuras

---
+++ src/main/java/com/gildedrose/BackstagePass.java
@@ -0,0 +1,8 @@
+package com.gildedrose;
+
+public class BackstagePass extends Item {
+
+    public BackstagePass( int sellIn, int quality) {
+        super(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT, sellIn, quality);
+    }
+}
--- src/test/java/com/gildedrose/TexttestFixture.java
+++ src/test/java/com/gildedrose/TexttestFixture.java
@@ -14,18 +14,18 @@ public class TexttestFixture {
 
         printStream.println("OMGHAI!");
 
-        Item[] items = new Item[] {
+        Item[] items = new Item[]{
                 new Item("+5 Dexterity Vest", 10, 20), //
-                new Item("Aged Brie", 2, 0), //
+                new AgedBrie(2, 0), //
                 new Item("Elixir of the Mongoose", 5, 7), //
-                new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
-                new Item("Sulfuras, Hand of Ragnaros", -1, 80),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 1, 20),
+                new Sulfuras(0, 80), //
+                new Sulfuras(-1, 80),
+                new BackstagePass(15, 20),
+                new BackstagePass(10, 49),
+                new BackstagePass(5, 49),
+                new BackstagePass(1, 20),
                 // this conjured item does not work properly yet
-                new Item("Conjured Mana Cake", 3, 6) };
+                new Item("Conjured Mana Cake", 3, 6)};
 
         GildedRose app = new GildedRose(items);
 
@@ -39,7 +39,7 @@ public class TexttestFixture {
             app.updateQuality();
         }
 
-        return  outputStream.toString();
+        return outputStream.toString();
     }
 
 }
